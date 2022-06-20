fb0bccd98993168b8af9342364fccf0185e69c7b
verah <verahnyagaka101@gmail.com>
2022-06-20 22:39:22 +0300

    commit tree 173e17d712eb6bee2347aa42d03325bf575e471e

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
-}
+
+    //  takes in a string and capitalizes the first letter
+    static capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+  
+    //takes in a string and removes all non-alphanumeric characters
+  
+    static sanitize( str) {
+      return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+    }
+  
+    //capitalizes all words in a sentence except pronouns
+    static titleize( sentence ) {
+      const exception = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+      let output = [];
+      let wordsArray = sentence.split( " " );
+  
+      for ( let i = 0; i < wordsArray.length; i++ ) {
+        if ( i == 0 ) {
+        output.push( this.capitalize( wordsArray[i] ) )
+        }
+        else {
+          if ( exception.includes( wordsArray[i] ) ) {
+            output.push( wordsArray[i] )}
+          else {
+            output.push( this.capitalize( wordsArray[i] ) )}
+        }
+      }
+      return output.join( " " );
+    }
+  }
